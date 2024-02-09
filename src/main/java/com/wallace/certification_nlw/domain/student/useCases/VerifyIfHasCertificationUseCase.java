e51e840d13a925caa1a9bada5e7d8d2e69664363
Wallace Martins <martinsswallace@gmail.com>
2024-02-09 16:06:34 -0300

    chore: verify has certification

--- src/main/java/com/wallace/certification_nlw/domain/student/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/wallace/certification_nlw/domain/student/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,15 +1,18 @@
 package com.wallace.certification_nlw.domain.student.useCases;
 
 import com.wallace.certification_nlw.domain.student.dto.VerifyHasCertificationDTO;
+import com.wallace.certification_nlw.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyHasCertificationDTO dto){
-        if (dto.email().equals("wallace@gmail.com") && dto.technology().equals("Java")){
-            return true;
-        }
-        return false;
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.email(), dto.technology());
+        return !result.isEmpty();
     }
 }
