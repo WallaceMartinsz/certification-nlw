eee34ee38e948e71477b62409389a7e3b2954de1
Wallace Martins <martinsswallace@gmail.com>
2024-02-09 16:05:03 -0300

    feat: create dtos

---
+++ src/main/java/com/wallace/certification_nlw/domain/questions/dto/AlternativesResultDTO.java
@@ -0,0 +1,20 @@
+package com.wallace.certification_nlw.domain.questions.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AlternativesResultDTO {
+
+    private UUID id;
+    private String description;
+}
